5f0ae2aecd275b14a58bfb1cc368c8ee36a74f5c
amaya-macarena <76908710+amaya-macarena@users.noreply.github.com>
2021-01-11 14:22:40 +0100

    Update run_asmc.py

--- CM1/run_asmc.py
+++ CM1/run_asmc.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Python 3.7 implementation of the Adaptive Sequential Monte Carlo method presented in Amaya et al. (2021).
+Python 3.7 implementation of the Adaptive Sequential Monte Carlo method presented in Amaya et al. (2021) [under review].
 This is a particle approach to infer the posterior probability density function and compute de evidence (marginal likelihood) 
 introduced by in Zhou et al (2016) [algorithm 4]. 
 
-This implementation (ASMC-DREAM) uses the code presented by Laloy et al. (2018a) for GAN-based probabilistic inversion using 
+This implementation (referred to as ASMC-DREAM) uses the code presented by Laloy et al. (2018a) for GAN-based probabilistic inversion using 
 DREAMzs MCMC sampler (ter Braak and Vrugt, 2008; Vrugt, 2009; Laloy and Vrugt, 2012). The associated synthetic cross-hole ground 
 penetrating radar (GPR) tomography data first-arrival times are calculated using the time2d algorithm by Podvin & Lecomte (1991).
 
@@ -35,10 +35,13 @@ References:
 
 Amaya, M., Linde, N., Laloy, E. Adaptive sequential Monte Carlo for posterior inference 
 and model selection among complex geological priors encoded with deep generative neural networks 
-[Submitted to Geophysical Journal International on December 2020]
+[Submitted to Geophysical Journal International on December 4th, 2020].
 
 Laloy, E., Hérault, R., Jacques, D., and Linde, N. 2018a. Training-image based geostatistical inversion using
 a spatial generative adversarial neural network. Water Resources Research, 54, 381–406. https://doi.org/10.1002/2017WR022148.
+
+Podvin, P. & Lecomte, I., 1991.  Finite difference computation of traveltimes in very contrasted velocity models: 
+a massively parallel approach and its associated tools,Geophysical Journal International,105(1), 271–284.
           
 Zhou,  Y.,  Johansen,  A.  M.,  &  Aston,  J.  A.,  2016.   Toward  automatic  model  comparison:  an adaptive sequential 
 Monte Carlo approach, Journal of Computational and Graphical Statistics,69925(3), 701–726.                       
