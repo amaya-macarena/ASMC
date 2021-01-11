6886d12052bd11c40575f5a3edfe2e60d76eb112
amaya-macarena <76908710+amaya-macarena@users.noreply.github.com>
2021-01-11 14:36:15 +0100

    Update run_asmc.py

--- CM1/run_asmc.py
+++ CM1/run_asmc.py
@@ -3,7 +3,7 @@
 Python 3.7 implementation of the Adaptive Sequential Monte Carlo method presented in Amaya et al. (under review), a particle approach 
 to infer the posterior probability density function and compute de evidence (marginal likelihood) introduced by in Zhou et al (2016) [algorithm 4]. 
 
-This implementation (reffered to as ASMC-DREAM) uses the code presented by Laloy et al. (2018a) for GAN-based probabilistic inversion using 
+This implementation (reffered to as ASMC-DREAM) uses the code presented by Laloy et al. (2018) for GAN-based probabilistic inversion using 
 DREAMzs MCMC sampler (ter Braak and Vrugt, 2008; Vrugt, 2009; Laloy and Vrugt, 2012). The associated synthetic cross-hole ground 
 penetrating radar (GPR) tomography data first-arrival times are calculated using the time2d algorithm by Podvin & Lecomte (1991).
 
@@ -11,7 +11,7 @@ In case you have any question and/or if you find a problem, please wirte me an e
 
 ===
 
-Copyright (C) 2018  Eric Laloy
+Copyright (C) 2018  Eric Laloy 
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -33,7 +33,7 @@ References:
 Amaya, M., Linde, N., Laloy, E. (under review). Adaptive sequential Monte Carlo for posterior inference and model selection among complex geological priors 
 encoded with deep generative neural networks [submitted to Geophysical Journal International on December 2020].
 
-Laloy, E., Hérault, R., Jacques, D., and Linde, N. 2018a. Training-image based geostatistical inversion using
+Laloy, E., Hérault, R., Jacques, D., and Linde, N. 2018. Training-image based geostatistical inversion using
 a spatial generative adversarial neural network. Water Resources Research, 54, 381–406. https://doi.org/10.1002/2017WR022148.
 
 Podvin, P. & Lecomte, I., 1991.  Finite difference computation of traveltimes in very contrasted velocity models: 
